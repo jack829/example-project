2fd6b3794c9dd33ef7f76d3c985f1dd04cecda2e
John Peterson <jlpeterson829@gmail.com>
2015-04-07 21:52:07 -0400

    Initialize backbone project

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Example, $*/
+
+
+window.Example = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Example.init();
+});
